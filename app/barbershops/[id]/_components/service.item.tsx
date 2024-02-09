5d5c41424b8347c77934691e2b572d9bf55c636e
José Carlos de Camargo <jccamargo15@gmail.com>
2024-02-09 18:22:39 -0300

    feat: add service booking

--- app/barbershops/[id]/_components/service.item.tsx
+++ app/barbershops/[id]/_components/service.item.tsx
@@ -4,27 +4,34 @@ import { useMemo, useState } from 'react'
 import Image from 'next/image'
 import { signIn } from 'next-auth/react'
 
-import { Service } from '@prisma/client'
+import { Barbershop, Service } from '@prisma/client'
 import { Button } from '@/app/_components/ui/button'
 import { Calendar } from '@/app/_components/ui/calendar'
 import { Card, CardContent } from '@/app/_components/ui/card'
 import {
   Sheet,
   SheetContent,
+  SheetFooter,
   SheetHeader,
   SheetTitle,
   SheetTrigger,
 } from '@/app/_components/ui/sheet'
 import { ptBR } from 'date-fns/locale'
 import { generateDayTimeList } from '../_helpers/hours'
+import { format } from 'date-fns/format'
 
 interface ServiceItemProps {
+  barbershop: Barbershop
   service: Service
   isAuthenticated?: boolean
 }
 
-const ServiceItem = ({ service, isAuthenticated }: ServiceItemProps) => {
-  const [date, setDate] = useState<Date | undefined>(new Date())
+const ServiceItem = ({
+  barbershop,
+  service,
+  isAuthenticated,
+}: ServiceItemProps) => {
+  const [date, setDate] = useState<Date | undefined>(undefined)
   const [hour, setHour] = useState<string | undefined>()
 
   const handleBooking = () => {
@@ -84,37 +91,39 @@ const ServiceItem = ({ service, isAuthenticated }: ServiceItemProps) => {
                     <SheetTitle>Fazer Reserva</SheetTitle>
                   </SheetHeader>
 
-                  <Calendar
-                    mode="single"
-                    selected={date}
-                    onSelect={setDate}
-                    className="mt-6"
-                    locale={ptBR}
-                    fromDate={new Date()}
-                    styles={{
-                      head_cell: {
-                        width: '100%',
-                        textTransform: 'capitalize',
-                      },
-                      cell: {
-                        width: '100%',
-                      },
-                      button: {
-                        width: '100%',
-                      },
-                      nav_button_previous: {
-                        width: '32px',
-                        height: '32px',
-                      },
-                      nav_button_next: {
-                        width: '32px',
-                        height: '32px',
-                      },
-                      caption: {
-                        textTransform: 'capitalize',
-                      },
-                    }}
-                  />
+                  <div className="py-6 px-5">
+                    <Calendar
+                      mode="single"
+                      selected={date}
+                      onSelect={handleDateClick}
+                      className="mt-6"
+                      locale={ptBR}
+                      fromDate={new Date()}
+                      styles={{
+                        head_cell: {
+                          width: '100%',
+                          textTransform: 'capitalize',
+                        },
+                        cell: {
+                          width: '100%',
+                        },
+                        button: {
+                          width: '100%',
+                        },
+                        nav_button_previous: {
+                          width: '32px',
+                          height: '32px',
+                        },
+                        nav_button_next: {
+                          width: '32px',
+                          height: '32px',
+                        },
+                        caption: {
+                          textTransform: 'capitalize',
+                        },
+                      }}
+                    />
+                  </div>
 
                   {/* Mostrar lista de horários apenas se uma data estiver selecionada */}
                   {date && (
@@ -124,13 +133,55 @@ const ServiceItem = ({ service, isAuthenticated }: ServiceItemProps) => {
                           key={time}
                           onClick={() => handleHourClick(time)}
                           variant={hour === time ? 'default' : 'outline'}
-                          className="rounded-full"
+                          className="rounded-full border-primary"
                         >
                           {time}
                         </Button>
                       ))}
                     </div>
                   )}
+
+                  <div className="py-6 px-5 border-t border-solid border-secondary">
+                    <Card>
+                      <CardContent className="flex flex-col gap-3 p-3">
+                        <div className="flex justify-between">
+                          <h2 className="font-bold">{service.name}</h2>
+                          <h3 className="font-bold text-sm">
+                            {Intl.NumberFormat('pt-BR', {
+                              style: 'currency',
+                              currency: 'BRL',
+                            }).format(Number(service.price))}
+                          </h3>
+                        </div>
+
+                        {date && (
+                          <div className="flex justify-between">
+                            <h3 className="text-gray-400 text-sm">Data</h3>
+                            <h4 className="text-sm">
+                              {format(date, "dd 'de' MM", {
+                                locale: ptBR,
+                              })}
+                            </h4>
+                          </div>
+                        )}
+
+                        {hour && (
+                          <div className="flex justify-between">
+                            <h3 className="text-gray-400 text-sm">Horário</h3>
+                            <h4 className="text-sm">{hour}</h4>
+                          </div>
+                        )}
+
+                        <div className="flex justify-between">
+                          <h3 className="text-gray-400 text-sm">Barbearia</h3>
+                          <h4 className="text-sm">{barbershop.name}</h4>
+                        </div>
+                      </CardContent>
+                    </Card>
+                  </div>
+                  <SheetFooter className="px-5">
+                    <Button disabled={!date || !hour}>Confirmar reserva</Button>
+                  </SheetFooter>
                 </SheetContent>
               </Sheet>
             </div>
