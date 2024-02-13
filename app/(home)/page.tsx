db40f5b395c71f21cb8f71116ccedc1905dc610d
José Carlos de Camargo <jccamargo15@gmail.com>
2024-02-13 16:30:50 -0300

    feat: ensure confirmed bookings are shown on home page

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -7,10 +7,29 @@ import Search from './_components/search'
 import BookingItem from '../_components/booking-item'
 import BarbershopItem from './_components/barbershop-item'
 import { db } from '../_lib/prisma'
+import { getServerSession } from 'next-auth'
+import { authOptions } from '../api/auth/[...nextauth]/route'
 
 export default async function Home() {
-  // chamar prisma e pegar barbearias
-  const barbershops = await db.barbershop.findMany({})
+  const session = await getServerSession(authOptions)
+
+  const [barbershops, confirmedBookings] = await Promise.all([
+    db.barbershop.findMany({}),
+    session?.user
+      ? db.booking.findMany({
+          where: {
+            userId: (session.user as any).id,
+            date: {
+              gte: new Date(),
+            },
+          },
+          include: {
+            service: true,
+            barbershop: true,
+          },
+        })
+      : Promise.resolve([]),
+  ])
 
   return (
     <div>
@@ -27,11 +46,15 @@ export default async function Home() {
         <Search />
       </div>
 
-      <div className="px-5 mt-6">
-        <h2 className="text-xs uppercase text-gray-400 font-bold mb-3">
+      <div className="mt-6">
+        <h2 className="pl-5 text-xs uppercase text-gray-400 font-bold mb-3">
           Agendamentos
         </h2>
-        {/* <BookingItem /> */}
+        <div className="flex gap-3 overflow-x-auto [&::-webkit-scrollbar]:hidden px-5">
+          {confirmedBookings.map((booking) => (
+            <BookingItem key={booking.id} booking={booking} />
+          ))}
+        </div>
       </div>
 
       <div className="mt-6">
--- app/_components/header.tsx
+++ app/_components/header.tsx
@@ -5,12 +5,15 @@ import { Card, CardContent } from './ui/card'
 import { Button } from './ui/button'
 import { Sheet, SheetContent, SheetTrigger } from './ui/sheet'
 import SideMenu from './side-menu'
+import Link from 'next/link'
 
 const Header = () => {
   return (
     <Card>
       <CardContent className="p-5 flex justify-between items-center">
-        <Image src="/logo.png" alt="FSW Barber" height={22} width={120} />
+        <Link href="/">
+          <Image src="/logo.png" alt="FSW Barber" height={22} width={120} />
+        </Link>
         <Sheet>
           <SheetTrigger asChild>
             <Button variant="outline" size="icon">
